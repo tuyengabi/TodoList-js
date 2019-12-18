a8051e18a2f6abd7038de0a0f2c4a435eff08b14
tuyen.gabi <tuyen.gabi@gmail.com>
2019-12-18 09:35:00 +0100

    modify script.js and index.html

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // add event switch light
 function toggleColor() {
-	var gradient = document.getElementById("gradient");
-	var currentClass = gradient.className;
+	const gradient = document.getElementById("gradient");
+	const currentClass = gradient.className;
 	gradient.className = currentClass == "gradient--darkmode" ? "gradient--lightmode" : "gradient--darkmode";
 };
 
 function toggleBgImg() {
-	var background = document.getElementById("bg");
-	var currentClass = background.className;
+	const background = document.getElementById("bg");
+	const currentClass = background.className;
 	background.className = currentClass == "bg--darkmode" ? "bg--lightmode" : "bg--darkmode";
 };
 
@@ -21,15 +21,15 @@ function toggleThemplate() {
 function addValueToList() {
 
 	//je récupere ma liste
-	let myList = document.getElementsByTagName('ul')[0];
+	const myList = document.getElementsByTagName('ul')[0];
 	// console.log(myList);
 
 	//je récupere ma item of la liste 
-	let newTask = document.createElement('li');
+	const newTask = document.createElement('li');
 	// console.log(newTask);
 
 	//je récupere la valeur d'input
-	let valueInput = document.getElementById('my-Input').value;
+	const valueInput = document.getElementById('my-Input').value;
 	// console.log(valueInput);
 
 
@@ -39,27 +39,27 @@ function addValueToList() {
 	} else {
 
 		//je créer et rempli la balise p du li
-		let newP = document.createElement('p');
+		const newP = document.createElement('p');
 		//j'ajoute le texte
 		newP.innerText = valueInput.charAt(0).toUpperCase() + valueInput.slice(1);
 		// j'ajoute la classe todoItem
 		newP.classList.add('todoItem');
 
 		//je créer et je rempli le label du li
-		let newLabel = document.createElement('label');
+		const newLabel = document.createElement('label');
 		//j'ajoute la class check a mon label
 		newLabel.classList.add('check');
 		//je créer l'input du label
-		let newLblInput = document.createElement('input');
+		const newLblInput = document.createElement('input');
 		//on force le type checkbox
 		newLblInput.setAttribute('type', 'checkbox');
 		//on force checked newLblInput.setAttribute('checked','checked');
 		//je créer le span du label
-		let newLblSpan = document.createElement('span');
+		const newLblSpan = document.createElement('span');
 		//on ajoute la classe checkmark
 		newLblSpan.classList.add('checkmark');
 		//je créer le p du label
-		let newLblP = document.createElement('p');
+		const newLblP = document.createElement('p');
 		//j'ajoute la classe status
 		newLblP.classList.add('status');
 		//j'ajoute le texte c'est fait
@@ -71,7 +71,7 @@ function addValueToList() {
 		newLabel.appendChild(newLblP);
 
 		//je créer le a du li
-		let newA = document.createElement('a');
+		const newA = document.createElement('a');
 		newA.setAttribute('href', '#');
 		newA.classList.add('close');
 		newA.addEventListener('click',function(){
@@ -93,7 +93,7 @@ function addValueToList() {
 
 };
 
-addValueToList();
+
 
 document.querySelectorAll('.close').forEach(element => {
 	element.addEventListener('click',function(){
@@ -101,10 +101,16 @@ document.querySelectorAll('.close').forEach(element => {
 	});
 });
 
-
+//event keypress addValueToList
 $(document).keypress(function(e){
 	if (e.key === 'Enter'){
 		e.preventDefault();
 		addValueToList();
     }
+});
+
+//animation of button switch light
+$('.toggle').click(function (e) {
+    e.preventDefault(); 
+    $(this).toggleClass('toggle-on');
 });
